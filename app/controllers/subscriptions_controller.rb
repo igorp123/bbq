e4a756920631bd83170700df296121d231fde92f
Igor <ig.plotnikov@gmail.com>
2019-01-11 02:29:57 +0300

    prohibit subscribtion on own event

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,10 +6,12 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.save
-      redirect_to @event, notice: I18n.t('controllers.subscription.created')
-    else
-      render 'events/show', alert: I18n.t('controllers.subscription.error')
+    unless current_user_can_edit?(@event)
+      if @new_subscription.save
+        redirect_to @event, notice: I18n.t('controllers.subscription.created')
+      else
+        render 'events/show', alert: I18n.t('controllers.subscription.error')
+      end
     end
   end
 
