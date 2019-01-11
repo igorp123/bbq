e69b94e2b38ebbc9438685cefd44cd215bab64b1
Igor <ig.plotnikov@gmail.com>
2019-01-11 04:34:02 +0300

    validate of unqq email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,13 +4,13 @@ class Subscription < ApplicationRecord
 
   validates :event, presence: true
 
-  validates :user_name, presence: true, unless: -> {user.present?}
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> {user.present?}
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
-  validates :user, uniqueness: {scope: :event_id}, if: -> {user.present?}
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> {user.present?}
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
-  validate :email_valid?
+  validate :email_valid?, unless: -> { user.present? }
 
   def user_name
     if user.present?
@@ -29,7 +29,7 @@ class Subscription < ApplicationRecord
   end
 
   def email_valid?
-    if user_email.present? && User.where(email: user_email).present? && user.blank?
+    if user_email.present? && User.where(email: user_email).present?
       errors.add(:email, I18n.t('subscriptions.subscription.email_busy'))
     end
   end
