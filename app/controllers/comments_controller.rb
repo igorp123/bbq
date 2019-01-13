c5a0015613ae2e3bcf24936fefe8ab030cff10d7
Igor <ig.plotnikov@gmail.com>
2019-01-13 12:34:04 +0300

    author do not receive email about his comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,6 +43,8 @@ end
     def notify_subscribers(event, comment)
       all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
+      all_emails -= [comment.user.email] if comment.user.present?
+
       all_emails.each do |mail|
         EventMailer.comment(event, comment, mail).deliver_now
       end
