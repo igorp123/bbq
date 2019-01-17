aaf8e2c45bbf9262f10d96c4e646582f070655e9
Igor <ig.plotnikov@gmail.com>
2019-01-17 12:25:57 +0300

    mailgun added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,7 +129,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'http://gobarbecue.herokuapp.com',
+  :domain         => 'gobarbecue.herokuapp.com',
   :authentication => :plain,
 }
 end
