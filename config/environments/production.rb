209dbf117db0c5002e80ec009962aa70c68ffc3b
Igor <ig.plotnikov@gmail.com>
2019-01-17 11:35:52 +0300

    sendgrid added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,22 +103,22 @@ Rails.application.configure do
   # отправка почты по протоколу SMTP
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    user_name: ENV['G_USERNAME'],
-    password: ENV['G_PASS'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
-  #   ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: '587',
+  #   user_name: ENV['G_USERNAME'],
+  #   password: ENV['G_PASS'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
   # }
+
+    ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
